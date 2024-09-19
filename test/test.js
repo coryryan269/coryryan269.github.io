c45ebab20abba4c9836d4e32ed96f67a64292b9a
Cory A <coryryan269@gmail.com>
2024-09-19 09:09:53 -0500

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,5 @@
+testDisplay = document.getElementById("testDisplay");
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(res => res.json())
+  .then(data => testDisplay.innerHTML = data);
