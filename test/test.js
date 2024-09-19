506650cab6d69f47545399546f9a18fb32e3265d
Cory A <coryryan269@gmail.com>
2024-09-19 09:43:58 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,4 +2,4 @@
 fetch("https://jsonplaceholder.typicode.com/users")
   .then(res => res.json())
   .then(data => testDisplay.innerHTML = JSON.stringify(data[0]));
-console.log("udpated);
+console.log("udpated");
