6c8f7154e0fcdd89de71ee40d4c8e586d98e2428
Cory A <coryryan269@gmail.com>
2024-09-19 09:29:31 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,4 +2,4 @@ testDisplay = document.getElementById("testDisplay");
 
 fetch("https://jsonplaceholder.typicode.com/users")
   .then(res => res.json())
-  .then(data => testDisplay.innerHTML = data[0]);
+  .then(data => testDisplay.innerHTML = JSON.stringify(data[0]));
